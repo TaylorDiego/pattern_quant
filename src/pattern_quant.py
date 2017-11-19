919d799d9da3c08bfbf90bd3cfa6f4917681d497
TaylorDiego <Hagin.Taylor@gmail.com>
2017-11-18 22:45:08 -0800

    add src and pattern_quant.py stub

---
+++ src/pattern_quant.py
@@ -0,0 +1,21 @@
+class Pattern_Quant(obj):
+    "identify amounts of pattern in image"
+
+    def __init__(self):
+        pass
+
+    def select_features():
+        """return features to be used for comparison"""
+        pass
+
+    def compare_feature():
+        """return similarity index (float)"""
+        pass
+
+    def decide_similarity_relation():
+        """returns bool if two items are more similar or different"""
+        pass
+
+    def recognize_group_of_three():
+        """returns bool of three similar items"""
+        pass
